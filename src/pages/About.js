a3e9f176f862e848c8b859f557a628a01056495e
Lee Berny Gifwandi <leesmacbookpro@lee.myfiosgateway.com>
2021-10-28 23:48:08 -0400

    fix1

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,6 +15,7 @@ function About(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
+  // eslint-disable-next-line 
   useEffect(() => getAboutData(), []);
 
   // define a function that will return the JSX needed once we get the data
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -15,14 +15,18 @@ function Projects(props) {
   };
 
   // make an initial call for the data inside a useEffect, so it only happens once on component load
+  // eslint-disable-next-line 
   useEffect(() => getProjectsData(), []);
 
   // define a function that will return the JSX needed once we get the data
+ 
   const loaded = () => {
     return projects.map((project) => (
       <div>
+        
         <h1>{project.name}</h1>
-        <img src={project.image} />
+        
+       <img src= {project.image} alt={project.image} />
         <a href={project.git}>
           <button>Github</button>
         </a>
